eb791138e89178d1317681e295bb3427d60a9411
Sean Lange <seanlange74@gmail.com>
2025-01-21 00:38:10 -0500

    Document shooter example

--- examples/shooter/shooter.ts
+++ examples/shooter/shooter.ts
@@ -12,16 +12,21 @@ const multyx = new MultyxServer(server);
 const activePlayers = new MultyxTeam("players");
 activePlayers.self.bullets = [];
 
+// Disallow changing of client values by the client
+// All client movement can be computed by the server
 multyx.on(Events.Connect, client => client.self.disable());
 
 multyx.on("join", (client, name) => {
+    // Ensure client name is not null, and does not already exist
     if(activePlayers.getClient(client.uuid) !== null) return false;
-
     if(typeof name !== "string") return false;
-
     if(Array.from(activePlayers.clients).find(x => x.self.name == name)) return false;
 
+    // Add client to MultyxTeam of active players
     activePlayers.addClient(client);
+
+    // Allow all active players to see self information
+    client.self.addPublic(activePlayers);
 
     client.self.name = name;
     client.self.x = Math.random() * 2000 - 1000;
@@ -32,23 +37,26 @@ multyx.on("join", (client, name) => {
     client.self.bulletDamage = 20;
     client.self.bulletSpeed = 300;
     
+    // Make client send event if pressing these keys
     client.controller.listenTo(["w", "a", "s", "d"]);
+
+    // Constrain client inside box
     client.self.x.min(-1000).max(1000);
     client.self.y.min(-1000).max(1000);
 
+    // Event to listen for shooting
     client.controller.listenTo(Input.MouseDown, (state) => {
-        if(!state.mouse.down) return;
-
         const direction = Math.atan2(
             state.mouse.y - client.self.y,
             state.mouse.x - client.self.x
         );
-
     
+        // Get cartesian values of speed
         const speed = client.self.bulletSpeed;
         const speedX = Math.cos(direction) * speed;
         const speedY = Math.sin(direction) * speed;
-    
+        
+        // Create a new bullet and push it to the activePlayers public object
         activePlayers.self.bullets.push({
             x: client.self.x,
             y: client.self.y,
@@ -61,6 +69,7 @@ multyx.on("join", (client, name) => {
     return true;
 });
 
+// Runs every update before information gets broadcast to clients
 multyx.on(Events.Update, () => {
     for(const { self, controller } of activePlayers.clients) {
         if(controller.state.keys["w"]) self.y += self.speed * multyx.deltaTime;
@@ -68,21 +77,26 @@ multyx.on(Events.Update, () => {
         if(controller.state.keys["s"]) self.y -= self.speed * multyx.deltaTime;
         if(controller.state.keys["d"]) self.x += self.speed * multyx.deltaTime;
     }
-/* 
+
     const bullets = activePlayers.self.bullets;
 
     for(let i=0; i<bullets.length; i++) {
+        bullets[i].x += bullets[i].speedX * multyx.deltaTime;
+        bullets[i].y += bullets[i].speedY * multyx.deltaTime;
+
         // Bullet went out of bounds
         if(Math.abs(bullets[i].x) > 1000
         || Math.abs(bullets[i].y) > 1000) {
             bullets.splice(i--, 1);
         }
 
+        // Check for collisions between bullet and all clients
         for(const { uuid, self } of activePlayers.clients) {
+            // Bullet can't hit same person who shot it
             if(bullets.client === uuid) continue;
 
-            const dx = Math.abs(self.x - bullets[i].x);
-            const dy = Math.abs(self.y - bullets[i].y);
+            const dx = self.x - bullets[i].x;
+            const dy = self.y - bullets[i].y;
 
             // Center of bullet is less than 10 units from center of player
             if(dx**2 + dy**2 < 10**2) {
@@ -98,5 +112,5 @@ multyx.on(Events.Update, () => {
                 bullets.splice(i--, 1);
             }
         }
-    } */
+    }
 });
